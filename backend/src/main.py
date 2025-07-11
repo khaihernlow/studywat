68f1be4619dc9f698498d5040c5d77539c9e18c5
Khaihern Low <khaihernlow@gmail.com>
2025-07-11 16:38:00 +0800

    pipeline run 4

--- backend/src/main.py
+++ backend/src/main.py
@@ -59,5 +59,5 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         content={"detail": exc.errors(), "body": exc.body},
     )
 
-# For AWS Lambda [run3]
+# For AWS Lambda [run4]
 handler = Mangum(app)
